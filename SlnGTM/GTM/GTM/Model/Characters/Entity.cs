348af5e239095351c0ba884a23f7f98381a70359
Fernando Lima <fernando.bueno.lima@gmail.com>
2013-02-26 17:26:44 -0300

    Adding the VS Solution

---
+++ SlnGTM/GTM/GTM/Model/Characters/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BXEL.Graphics;
+
+namespace GTM.Model.Characters
+{
+    class Entity : Sprite
+    {
+        #region Properties
+
+        public String Name { get; set; }
+
+        public MapLocation Location { get; private set; }
+
+        #endregion
+    }
+}
---
+++ SlnGTM/GTM/GTM/Model/MapLocation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GTM.Model
+{
+    class MapLocation
+    {
+        #region Properties
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public MapLocation(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public MapLocation() : this(0,0) { }
+
+        #endregion
+
+        #region Methods
+
+        //public void CenterLocation(Map map) { }
+
+        public void WalkX(int steps)
+        {
+            X += steps;
+        }
+
+        public void WalkX()
+        {
+            WalkX(1);
+        }
+
+        public void WalkY(int steps)
+        {
+            Y += steps;
+        }
+
+        public void WalkY()
+        {
+            WalkY(1);
+        }
+
+        #endregion
+    }
+}
