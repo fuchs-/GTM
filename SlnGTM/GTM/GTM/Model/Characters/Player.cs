cfffea215f536cb6baa0b3e398ffa15bf4daa728
Fernando Bueno de Lima <fernando.bueno.lima@gmail.com>
2013-03-08 19:11:04 -0300

    Added Player and Team classes

---
+++ SlnGTM/GTM/GTM/Model/Characters/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GTM.Model.Characters
+{
+    public class Player
+    {
+        #region Properties
+
+        public Hero CurrentHero { get; private set; }
+        
+        public Team CurrentTeam { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        public void AddedToTeam(Team team)
+        {
+            CurrentTeam = team;
+        }
+
+        #endregion
+    }
+}
---
+++ SlnGTM/GTM/GTM/Model/Characters/Team.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GTM.Model.Characters
+{
+    public enum TeamColor { Blue, Red }
+
+    public class Team
+    {
+        #region Properties
+
+        private List<Player> Players { get; set; }
+
+        public TeamColor Color { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Team(TeamColor color)
+        {
+            Players = new List<Player>();
+            Color = color;
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region Hero management
+
+        public void AddPlayer(Player player)
+        {
+            Players.Add(player);
+            
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
